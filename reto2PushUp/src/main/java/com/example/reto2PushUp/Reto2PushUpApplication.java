c2d35aae4be8fd984a382093977a4d5d85ea0d3d
KazeVentum <kazeventum@gmail.com>
2024-04-23 06:39:35 -0500

    feat: :tada: Se crean archivos necesarios para el reto

---
+++ reto2PushUp/src/main/java/com/example/reto2PushUp/Reto2PushUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto2PushUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2PushUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2PushUpApplication.class, args);
+	}
+
+}
